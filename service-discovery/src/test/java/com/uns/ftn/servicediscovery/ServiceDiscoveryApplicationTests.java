58ab362b36b3abf704ff12cdfe2fbb47c62b1b4b
Boris Opačić <57530291+borisop@users.noreply.github.com>
2020-05-19 15:39:43 +0200

    Added generated services to service structure for project.
    Signed-off-by: Boris Opačić <57530291+borisop@users.noreply.github.com>

---
+++ service-discovery/src/test/java/com/uns/ftn/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uns.ftn.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
