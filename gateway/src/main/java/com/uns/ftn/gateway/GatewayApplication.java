b892a28c7864667656021768851d460971032046
petkalitis <dusan.petkovic02@gmail.com>
2020-05-25 13:42:08 +0200

    Zuul initial https config

--- gateway/src/main/java/com/uns/ftn/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/uns/ftn/gateway/GatewayApplication.java
@@ -1,19 +1,37 @@
 package com.uns.ftn.gateway;
 
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
 public class GatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
+		DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
+		System.setProperty("javax.net.ssl.keyStore", "keystore.p12");
+		System.setProperty("javax.net.ssl.keyStorePassword", "password");
+		System.setProperty("javax.net.ssl.trustStore", "truststore.p12");
+		System.setProperty("javax.net.ssl.trustStorePassword", "password");
+		EurekaJerseyClientImpl.EurekaJerseyClientBuilder builder = new EurekaJerseyClientImpl.EurekaJerseyClientBuilder();
+		builder.withClientName("zuul");
+		builder.withSystemSSLConfiguration();
+		builder.withMaxTotalConnections(10);
+		builder.withMaxConnectionsPerHost(10);
+		args.setEurekaJerseyClient(builder.build());
+		return args;
 	}
 
 }
