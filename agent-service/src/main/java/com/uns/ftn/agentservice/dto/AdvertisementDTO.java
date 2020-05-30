8cdc12eb6398a7c50e17126b760002236a515174
petkalitis <dusan.petkovic02@gmail.com>
2020-05-30 23:01:56 +0200

    Backend adding images
    Handled adding images sent from client app through multipart/form-data
    Provided API for getting images from server

--- agent-service/src/main/java/com/uns/ftn/agentservice/dto/AdvertisementDTO.java
+++ agent-service/src/main/java/com/uns/ftn/agentservice/dto/AdvertisementDTO.java
@@ -13,25 +13,25 @@ import java.util.Set;
 @AllArgsConstructor
 public class AdvertisementDTO {
 
+    private Long id;
     private double price;
     private double kilometersPerDayLimit = -1;
     private Boolean collisionDamageWaiver = false;
     private String description;
     private Vehicle vehicle;
     private Long ownerId;
-    private Set<Photo> photos;
     private double rating = 0;
     private Set<RentingInterval> rentingIntervals;
     private Set<Comment> comments;
 
     public AdvertisementDTO(Advertisement ad) {
+        this.id = ad.getId();
         this.price = ad.getPrice();
         this.kilometersPerDayLimit = ad.getKilometersPerDayLimit();
         this.collisionDamageWaiver = ad.getCollisionDamageWaiver();
         this.description = ad.getDescription();
         this.vehicle = ad.getVehicle();
         this.ownerId = ad.getOwnerId();
-        this.photos = ad.getPhotos();
         this.rating = ad.getRating();
         this.rentingIntervals = ad.getRentingIntervals();
         this.comments = ad.getComments();
