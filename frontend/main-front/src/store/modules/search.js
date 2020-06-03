8f08aa9a324811084a1dc3cc544c63bacddd8986
Boris Opačić <57530291+borisop@users.noreply.github.com>
2020-06-03 14:20:13 +0200

    Added search component to frontend.
    Signed-off-by: Boris Opačić <57530291+borisop@users.noreply.github.com>

---
+++ frontend/main-front/src/store/modules/search.js
@@ -0,0 +1,24 @@
+import searchApi from "@/api/Search.js";
+
+const state = {
+
+};
+
+const getters = {
+
+};
+
+const actions = {
+
+};
+
+const mutations = {
+
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
