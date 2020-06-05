cd6f9cd2c9c05782896d01b06bdb178cd0bc5ee5
petkalitis <dusan.petkovic02@gmail.com>
2020-06-05 22:41:51 +0200

    Configuring docker
    Configuring docker-compose file so that it works

--- database/schemas.sql
+++ database/schemas.sql
@@ -2,3 +2,5 @@ CREATE SCHEMA account_schema AUTHORIZATION postgres;
 CREATE SCHEMA agent_schema AUTHORIZATION postgres;
 CREATE SCHEMA catalog_schema AUTHORIZATION postgres;
 CREATE SCHEMA renting_schema AUTHORIZATION postgres;
+CREATE SCHEMA search_schema AUTHORIZATION postgres;
+CREATE SCHEMA view_schema AUTHORIZATION postgres;
