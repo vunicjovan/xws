600da1cd6836e3a710f7cc7ce3ff12e87ce0a57e
Jovan Vunić <53936065+vunicjovan@users.noreply.github.com>
2020-06-20 00:14:24 +0200

    New chat component for main client.

--- frontend/main-front/src/views/chat/Chat.vue
+++ frontend/main-front/src/views/chat/Chat.vue
@@ -1,76 +1,207 @@
 <template>
-    <chat-window height="680px" :currentUserId="currentUserId" :rooms="rooms" :messages="messages" theme="dark" />
+    <div>
+        <md-app>
+            <md-app-toolbar class="md-transparent">
+                <span class="md-title">{{ this.selectedTitle }}</span>
+            </md-app-toolbar>
+
+            <md-app-drawer md-permanent="full">
+                <md-toolbar class="md-transparent" md-elevation="0">
+                    Active chat rooms
+                </md-toolbar>
+
+                <md-list>
+                    <div v-for="room in rooms" v-bind:key="room.id">
+                        <md-divider></md-divider>
+                        <md-list-item @click="setRoom(room)" class="md-button">
+                            <span class="md-list-item-button">{{ room.name }}</span>
+                        </md-list-item>
+                        <md-divider></md-divider>
+                    </div>
+                </md-list>
+            </md-app-drawer>
+            
+            <md-app-content>
+                <div class="msgDiv" v-if="this.selectedRoom != null && this.currentMessages !== null">
+                    <div v-for="msg in currentMessages" v-bind:key="getMsgIndex(msg)">
+                        <div v-if="msg.senderID === 2" style="text-align: right;">
+                            <span class="message-card-mine">{{ msg.content }}</span>
+                        </div>
+                        <div v-else>
+                            <span class="message-card">{{ msg.content }}</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div v-else style="text-align: center;">
+                    <md-empty-state md-label="No opened conversations"></md-empty-state>
+                </div>
+                
+                <md-field :class="{ 'md-invalid': $v.currentMessage.$error }" md-clearable v-if="this.currentMessages !== null">
+                    <label>Send message</label>
+                    <md-input v-on:keyup.enter="sendMessage" v-model="currentMessage" class="textara" md-autogrow maxlength="120"></md-input>
+                    <span class="md-error" v-if="!$v.currentMessage.required">Invalid message format</span>
+                </md-field>
+            </md-app-content>
+
+        </md-app>
+  </div>
 </template>
 
 <script>
-import ChatWindow from 'vue-advanced-chat'
-import 'vue-advanced-chat/dist/vue-advanced-chat.css'
+import { mapGetters } from "vuex";
+import { validationMixin } from "vuelidate";
+import { required, integer, decimal, helpers } from "vuelidate/lib/validators";
+
+const sqli = helpers.regex("alpha", /^(?!script|select|from|where|SCRIPT|SELECT|FROM|WHERE|Script|Select|From|Where)([a-zA-Z0-9!?#.,:;\s?]+)$/);
 
 export default {
     name: "Chat",
-    components: {
-      ChatWindow
-    },
-    data() {
+    mixins: [validationMixin],
+    data: function() {
         return {
+            selectedRoom: null,
+            selectedTitle: "",
+            currentMessage: "",
+            currentMessages: null,
+            msgCounter: 0,
             rooms: [
                 {
-                    roomId: 1,
-                    roomName: 'Room 1',
-                    lastMessage: {
-                        content: 'Some random message',
-                        sender_id: 1234,
-                        username: 'John Doe',
-                        timestamp: '10:20',
-                        date: 123242424,
-                        seen: false,
-                        new: true
-                    },
-                    users: [
+                    "id": 1,
+                    "name": "Agent Pera",
+                    "messages": [
                         {
-                            _id: 1234,
-                            username: 'John Doe',
-                            status: {
-                            state: 'online',
-                            last_changed: 'today, 14:30'
-                            }
+                            "id": 1,
+                            "senderID": 1,
+                            "receiverID": 2,
+                            "content": "Hey, man",
                         },
                         {
-                            _id: 4321,
-                            username: 'John Snow',
-                            status: {
-                            state: 'offline',
-                            last_changed: '14 July, 20:00'
-                            }
+                            "id": 2,
+                            "senderID": 2,
+                            "receiverID": 1,
+                            "content": "How are you?"
                         }
-                    ],
-                    typingUsers: [ 4321 ]
-                }
-            ],
-            messages: [
+                    ]
+                },
                 {
-                    _id: 7890,
-                    content: 'This is crazy!',
-                    sender_id: 1234,
-                    username: 'John Doe',
-                    date: '13 November',
-                    timestamp: '10:20',
-                    seen: true,
-                    disable_actions: false,
-                    disable_reactions: false,
-                    reactions: {
-                        laughing: [
-                            1234
-                        ]
-                    }
+                    "id": 2,
+                    "name": "Agent Mika",
+                    "messages": []
                 }
-            ],
-            currentUserId: 1234,
+            ]
         }
-    }
+    },
+    computed: mapGetters(["isLogged", "getUser"]),
+    methods: {
+        setRoom(room) {
+            this.selectedRoom = room;
+            this.selectedTitle = "Conversation with " + this.selectedRoom.name;
+            this.currentMessages = this.selectedRoom.messages;
+            this.msgCounter = this.currentMessages.length;
+        },
+
+        getMsgIndex(msg) {
+            return this.currentMessages.indexOf(msg);
+        },
+
+        sendMessage() {
+            this.$v.$touch();
+            if (this.selectedTitle !== "" && !this.$v.$invalid && this.currentMessage !== "") {
+                this.msgCounter = this.msgCounter + 1;
+                var msg = {
+                    "senderID": this.getUser.id,
+                    "receiverID": this.selectedRoom.id,
+                    "content": this.currentMessage,
+                    "username": this.getUser.firstName + " " + this.getUser.lastName 
+                };
+                console.log(msg);
+
+                // send message to server
+
+                this.currentMessages.push(msg);
+                this.currentMessage = "";
+            }
+        }
+    },
+    validations: {
+		currentMessage: {
+			sqli
+		}
+	}
 }
 </script>
 
 <style>
+    .md-app .md-app-drawer {
+        width: 40%;
+        padding: 2%;
+    }
+
+    .md-app {
+        padding: 2%;
+        height: 500px;
+    }
+
+    .md-app .md-list .md-list-item {
+        width: 95%;
+    }
+
+    .md-app-content .md-field {
+        position: fixed;
+        bottom: 0.01%;
+        width: 85%;
+    }
+
+    .textara {
+        max-width: 90%;
+    }
+
+    .message-card {
+        padding: 1.5%;
+        border: 2.5px solid rgb(115, 153, 179);
+        display: inline-block;
+        margin-bottom: 2.5%;
+        margin-right: 55%;
+        word-break: break-word;
+        border-radius: 15px;
+    }
+
+    .message-card-mine {
+        padding: 1.5%;
+        border: 2.5px solid #1C6EA4;
+        display: inline-block;
+        margin-bottom: 2.5%;
+        margin-left: 55%;
+        word-break: break-word;
+        border-radius: 15px;
+    }
+
+    .md-app-content .msgDiv {
+        height: 290px;
+        width: 80%;
+        overflow-y: auto;
+        padding: 1%;
+        margin: 0 auto;
+    }
+
+    /* width */
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+        background: #f1f1f1; 
+    }
     
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: #888; 
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        background: #555; 
+    }
 </style>
