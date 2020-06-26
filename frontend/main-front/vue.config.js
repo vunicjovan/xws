00b7823a998884e71c74649a8a28fb02a51b7762
Dusan <dusan.petkovic02@gmail.com>
2020-06-26 19:56:49 +0200

    SOAP https
    Configuring soap client and establishing successful SSLHandshake with microservices

--- frontend/main-front/vue.config.js
+++ frontend/main-front/vue.config.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 module.exports = {
 	devServer: {
 		port: 8090,
-		https: true,
+		https: {
+			cert: fs.readFileSync("./src/client.crt"),
+			key: fs.readFileSync("./src/client.key"),
+		},
 	},
 };
